1d2ce06ddaa9031ace0a8fd260084e7e2ccf4726
Salvador Romero Figueroa <salvaromero2016@gmail.com>
2025-06-06 12:33:20 -0300

    renamed:    backend1/app/url_mapping.go -> backend/app/url_mapping.go 	renamed:    backend1/bd/bd.go -> backend/bd/bd.go 	renamed:    backend1/bd/script.sql -> backend/bd/script.sql 	renamed:    backend1/domain/actividad.go -> backend/domain/actividad.go 	new file:   backend/domain/inscripcion.go 	renamed:    backend1/domain/usuario.go -> backend/domain/usuario.go 	renamed:    backend1/dto/dto_actividad.go -> backend/dto/dto_actividad.go 	new file:   backend/dto/dto_inscripcion.go 	renamed:    backend1/dto/dto_usuario.go -> backend/dto/dto_usuario.go 	renamed:    backend1/go.mod -> backend/go.mod 	renamed:    backend1/go.sum -> backend/go.sum 	new file:   backend/main.go 	deleted:    backend1/domain/inscripcion.go 	deleted:    backend1/dto/dto_inscripcion.go 	deleted:    backend1/main.go

---
+++ backend/domain/inscripcion.go
@@ -0,0 +1,14 @@
+package domain
+
+import "time"
+
+type Inscripcion struct {
+	ID          int `gorm:"primaryKey"`
+	UsuarioID   int `gorm:"foreignKey:UsuarioID"`
+	Usuario     Usuario
+	Fecha       time.Time `gorm:"type:date"`
+	ActividadID int       `gorm:"foreignKey:ActividadID"`
+	Actividad   Actividad
+}
+
+type Inscrpciones []Inscripcion
