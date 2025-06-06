21c305a10427cf3de5e674c83e327ddde087c55c
pablovenica <127855050+pablovenica@users.noreply.github.com>
2025-06-06 12:39:32 -0300

    Update dto_actividad.go

--- backend/dto/dto_actividad.go
+++ backend/dto/dto_actividad.go
@@ -1,17 +1,15 @@
 package dto
 
-
 type ActividadDto struct {
-	ID 			int 	`json:nombre`
-	Nombre 		string  `json:nombre`
-	Descripcion string 	`json:descripcion`
-	Estado 		bool 	`json:estado`
-	Cupo 		int 	`json:cupo`
-	Disponible	int 	`json:disponible`
-	Profesor 	string 	`json:profesor`
-	Horario 	string 	`json:horario`
-	Categoria   string 	`json:categoria_id`
+	ID         int    `json:"id"`
+	Nombre     string `json:"nombre"`
+	Descripcion string `json:"descripcion"`
+	Estado     bool   `json:"estado"`
+	Cupo       int    `json:"cupo"`
+	Disponible int    `json:"disponible"`
+	Profesor   string `json:"profesor"`
+	Horario    string `json:"horario"`
+	Categoria  string `json:"categoria"`
 }
 
-
-type ActividadesDto [] ActividadDto
+type ActividadesDto []ActividadDto
