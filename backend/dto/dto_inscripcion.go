967c9c3b9198dbb154f459dccc05b1bad606a47d
pablovenica <127855050+pablovenica@users.noreply.github.com>
2025-06-06 12:39:52 -0300

    Update dto_inscripcion.go

--- backend/dto/dto_inscripcion.go
+++ backend/dto/dto_inscripcion.go
@@ -1,10 +1,12 @@
 package dto
 
+import "time"
 
 type InscripcionDto struct {
-	ID 			int 		`json:id`
-	UsuarioId 	int 		`json:usuario_id`
-	Fecha 		time.Time	`json:date`
-	ActividadID int 		`json:actividad_id`
+	ID          int       `json:"id"`
+	UsuarioId   int       `json:"usuario_id"`
+	Fecha       time.Time `json:"fecha"`
+	ActividadID int       `json:"actividad_id"`
 }
-type InscrpcionesDto[] InscripcionDto
+
+type InscripcionesDto []InscripcionDto
