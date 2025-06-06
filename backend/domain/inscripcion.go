cddd42025194d572a86bc593d8cba8395b989163
pablovenica <127855050+pablovenica@users.noreply.github.com>
2025-06-06 12:38:32 -0300

    Update inscripcion.go

--- backend/domain/inscripcion.go
+++ backend/domain/inscripcion.go
@@ -1,15 +1,14 @@
 package domain
+
 import "time"
+
 type Inscripcion struct {
-	ID           int        `gorm:"primaryKey"`
-	UsuarioID    int        `gorm:"foreignKey:UsuarioID`
-	Usuario      Usuario    
-	Fecha        time.Time 	`gorm:"type:date"`
-	ActividadID  int        `gorm:"foreignKey:ActividadID`
-	Actividad    Actividad  
+	ID          int       `gorm:"primaryKey"`
+	UsuarioID   int       `gorm:"column:usuario_id"`
+	Usuario     Usuario   `gorm:"foreignKey:UsuarioID"`
+	Fecha       time.Time `gorm:"type:date"`
+	ActividadID int       `gorm:"column:actividad_id"`
+	Actividad   Actividad `gorm:"foreignKey:ActividadID"`
 }
 
-
-type Inscrpciones[] Inscripcion
-
-
+type Inscripciones []Inscripcion
