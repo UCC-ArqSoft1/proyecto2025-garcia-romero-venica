6e845f90cda195dd17eca272babc2b0629212034
pablovenica <127855050+pablovenica@users.noreply.github.com>
2025-06-11 16:14:07 -0300

    Update usuario.go

--- backend/domain/usuario.go
+++ backend/domain/usuario.go
@@ -4,8 +4,8 @@ type Usuario struct {
 	ID          int    `gorm:"column:id_usuarios;primaryKey;autoIncrement"`
 	Nombre      string `gorm:"type:varchar(45);not null"`
 	Email       string `gorm:"type:varchar(45);unique;not null"`
-	Password    string `gorm:"type:varchar(100);not null"`
-	TipoUsuario string `gorm:"type:bool"`
+	Password    string `gorm:"column:password;type:varchar(100);not null"`
+	TipoUsuario string `gorm:"column:tipo_usuarios_id;type:tinyint"`
 }
 
 type Usuarios []Usuario
